97ac8e0dfb1229fa2ca1bceedcdf7a02fe4949de
Sergio Cabral <git@sergiocabral.com>
2022-08-18 14:12:19 -0300

    Comunicação entre Cliente e Servidor

--- AppClient.cs
+++ AppClient.cs
@@ -1,14 +1,37 @@
-﻿namespace TelnetClientServer;
+﻿using System.Net.Sockets;
+
+namespace TelnetClientServer;
 
 public class AppClient
 {
+    private const string LogPrefix = $"[ {nameof(AppClient)} ]";
+    
+    private readonly string _hostname;
+    private readonly int _port;
+    private readonly TcpClient _tcpClient;
+
     public AppClient(string hostname, int port)
     {
-        throw new NotImplementedException();
+        _hostname = hostname;
+        _port = port;
+        _tcpClient = new TcpClient();
+        
+        Console.WriteLine($"{LogPrefix} Created to {hostname}:{port}.");
     }
 
     public void Start()
     {
-        throw new NotImplementedException();
+        Console.WriteLine($"I'm a client.");
+        
+        _tcpClient.Connect(_hostname, _port);
+
+        var writer = new StreamWriter(_tcpClient.GetStream());
+        writer.AutoFlush = true;
+        writer.WriteLine("Hello! I'm from client.");
+
+        var reader = new StreamReader(_tcpClient.GetStream());
+        Console.WriteLine(reader.ReadLine());
+        
+        _tcpClient.Close();
     }
 }
--- AppServer.cs
+++ AppServer.cs
@@ -1,14 +1,44 @@
-﻿namespace TelnetClientServer;
+﻿using System.Diagnostics;
+using System.Net;
+using System.Net.Sockets;
+
+namespace TelnetClientServer;
 
 public class AppServer
 {
+    private const string LogPrefix = $"[ {nameof(AppServer)} ]";
+    
+    private readonly TcpListener _tcpListener;
+    private readonly ProcessStartInfo _processStartInfo;
+
     public AppServer(string executable, int port)
     {
-        throw new NotImplementedException();
+        _tcpListener = new TcpListener(IPAddress.Any, port);
+        _processStartInfo = new ProcessStartInfo(executable)
+        {
+            RedirectStandardInput = true,
+            RedirectStandardOutput = true,
+            RedirectStandardError = true,
+            UseShellExecute = false
+        };
+
+        Console.WriteLine($"{LogPrefix} Created. Executable \"{executable}\" to port {port}.");
     }
 
     public void Start()
     {
-        throw new NotImplementedException();
+        Console.WriteLine("I'm a SERVER.");
+        
+        _tcpListener.Start();
+        var tcpClient = _tcpListener.AcceptTcpClient();
+
+        var writer = new StreamWriter(tcpClient.GetStream());
+        writer.AutoFlush = true;
+        writer.WriteLine("Hello! I'm from server.");
+
+        var reader = new StreamReader(tcpClient.GetStream());
+        Console.WriteLine(reader.ReadLine());
+        
+        tcpClient.Close();
     }
 }
