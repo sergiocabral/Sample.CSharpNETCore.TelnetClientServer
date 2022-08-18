b54f20cece15f657eaeb94cd61280b355665df65
Sergio Cabral <git@sergiocabral.com>
2022-08-18 14:01:23 -0300

    Usando argumentos para executar como Cliente ou Servidor

--- Program.cs
+++ Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using System.Text.RegularExpressions;
+
+var regexMatchNameAndPort = new Regex(@"([^\s:]+):(\d+)").Match(string.Join(' ', args));
+
+var name = regexMatchNameAndPort.Success ? regexMatchNameAndPort.Groups[1].Value : null;
+
+var port = regexMatchNameAndPort.Success ? int.Parse(regexMatchNameAndPort.Groups[2].Value) : 23;
+
+if (args.Contains("--server")) {
+    var executable = name ?? "cmd.exe";
+    Console.WriteLine($"Server: {executable}:{port}");
+} else {
+    var hostname = name ?? "localhost";
+    Console.WriteLine($"Client: {hostname}:{port}");
+}
